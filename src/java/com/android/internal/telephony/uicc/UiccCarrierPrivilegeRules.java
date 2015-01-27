cefb806ca891c7d9a0fdd6002492d752bbf38c24
mickybart <mickybart@pygoscelis.org>
2015-01-27 11:26:01 -0500

    UiccCarrierPrivilegeRules: Fake loaded
    To avoid rild crash

--- src/java/com/android/internal/telephony/uicc/UiccCarrierPrivilegeRules.java
+++ src/java/com/android/internal/telephony/uicc/UiccCarrierPrivilegeRules.java
@@ -186,8 +186,10 @@ public class UiccCarrierPrivilegeRules extends Handler {
         mLoadedCallback = loadedCallback;
 
         // Start loading the rules.
-        mUiccCard.iccOpenLogicalChannel(AID,
-            obtainMessage(EVENT_OPEN_LOGICAL_CHANNEL_DONE, null));
+        //mUiccCard.iccOpenLogicalChannel(AID,
+        //    obtainMessage(EVENT_OPEN_LOGICAL_CHANNEL_DONE, null));
+        mAccessRules = new ArrayList<AccessRule>();
+        updateState(STATE_LOADED);
     }
 
     /**
